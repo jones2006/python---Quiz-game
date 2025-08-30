93f51a6eaf0f8353de3e6202664aedc79174584c
jones2006 <jones.j8637@gmail.com>
2025-08-30 19:58:11 +0530

    project complete2 commit

--- Quiz-game(jones).py
+++ Quiz-game(jones).py
@@ -11,19 +11,19 @@ print("Okay,start a game :) ")
 
 # question 1
 qns_1 = input("CS full form :")
-ans_1 = "correct" if qns_1 == "Computer science" else "incorrect"
+ans_1 = "correct" if qns_1.lower() == "computer science" else "incorrect"
 print(ans_1)
 
 
 # question 2
 qns_2 = input("CPU full form :")
-ans_2 = "correct" if qns_2 == "Central processing unit" else "incorrect"
+ans_2 = "correct" if qns_2.lower() == "central processing unit" else "incorrect"
 print(ans_2)
 
 
 # question 3
 qns_3 = input("GPU full form :")
-ans_3 = "correct" if qns_3 == "Graphics processing unit" else "incorrect"
+ans_3 = "correct" if qns_3.lower() == "graphics processing unit" else "incorrect"
 print(ans_3)
 
 print("Thank you for playing!")
