2f0dedf19a52cf33a760840778160d48c8dcdeff
jones2006 <jones.j8637@gmail.com>
2025-08-30 20:40:32 +0530

    project complete 3

--- Quiz-game(jones).py
+++ Quiz-game(jones).py
@@ -1,5 +1,6 @@
 print("Welcome to a Quiz game")
 playgame = input("Are you play a game ?")
+score = 0
 
 if playgame.lower() != "yes":
     print("Okay,Exit a game :( ")
@@ -7,23 +8,30 @@ if playgame.lower() != "yes":
 
 print("Okay,start a game :) ")
 
-# one line if else syntax "value_if_true if <condition> else value_if_false"
 
-# question 1
 qns_1 = input("CS full form :")
-ans_1 = "correct" if qns_1.lower() == "computer science" else "incorrect"
-print(ans_1)
+if qns_1.lower() == "computer science":
+    print("correct")
+    score += 1
+else:
+    print("incorrect")
 
 
-# question 2
 qns_2 = input("CPU full form :")
-ans_2 = "correct" if qns_2.lower() == "central processing unit" else "incorrect"
-print(ans_2)
+if qns_2.lower() == "central processing unit":
+    print("correct")
+    score += 1
+else:
+    print("incorrect")
 
 
-# question 3
 qns_3 = input("GPU full form :")
-ans_3 = "correct" if qns_3.lower() == "graphics processing unit" else "incorrect"
-print(ans_3)
+if qns_3.lower() == "graphics processing unit":
+    print("correct")
+    score += 1
+else:
+    print("incorrect")
 
+print("your score is "+str(score) + " and " +
+      "Percentage is " + str((score/3) * 100) + "%")
 print("Thank you for playing!")
